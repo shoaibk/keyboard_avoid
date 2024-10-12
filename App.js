e9172a81de40b6a837da128c9ba8f0478ac141ca
Shoaib Khan <shb.khan@gmail.com>
2024-10-12 09:32:28 -0400

    Keyboard avoiding view

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  StyleSheet,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  SafeAreaView,
+} from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+const KeyboardAvoidingComponent = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.header}>KeyboardAvoidingView</Text>
+          <TextInput placeholder="First Name" style={styles.textInput} />
+          <TextInput placeholder="Last Name" style={styles.textInput} />
+          <TextInput placeholder="Email" style={styles.textInput} />
+          <TextInput placeholder="Street Address" style={styles.textInput} />
+          <TextInput placeholder="Postal Code" style={styles.textInput} />
+
+          <View style={styles.btnContainer}>
+            <Button title="Submit" onPress={() => null} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  header: {
+    fontSize: 26,
+    marginTop: 38,
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    paddingTop: 8,
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12,
   },
 });
+
+export default KeyboardAvoidingComponent;
